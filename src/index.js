845824f7148924c5ed2935aa049fa3d59c8a44c2
p17166907 <56296788+p17166907@users.noreply.github.com>
2022-07-27 20:02:43 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Evan";
+const theNumber = 8;
+
+console.log({ name }); // prints: {name: "Evan"}
+console.log(name); // prints: Evan
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Your lucky number is {theNumber}</p>
+  </div>,
+  document.getElementById("root")
+);
