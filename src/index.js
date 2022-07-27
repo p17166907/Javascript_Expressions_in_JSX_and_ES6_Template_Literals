0395a614cfd0a632aef5d54096f97ce3cd1d0e0f
p17166907 <56296788+p17166907@users.noreply.github.com>
2022-07-27 20:28:53 +0100

    JSX and Template literals

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Evan";
+const fName = "Evan";
+const lName = "Jordan";
 const theNumber = 8;
 
-console.log({ name }); // prints: {name: "Evan"}
-console.log(name); // prints: Evan
+console.log({ fName }); // prints: {name: "Evan"}
+console.log(fName); // prints: Evan
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {`${fName} ${lName}`}!</h1>
     <p>Your lucky number is {theNumber}!!!</p>
+    <p>3 + 3 = {3 + 3}</p>
+    <p>Random number => {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
